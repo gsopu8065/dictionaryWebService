8c8e8139bcc4131b5735bb2c635a33b7d6798796
srujangopu <srujanjack@gmail.com>
2016-06-14 21:54:51 -0500

    modified proc file

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.listen(9050, function () {
+app.listen(process.env.PORT || 5000, function () {
     console.log("Example app listening at http://localhost:%s", 9050)
 });
 
