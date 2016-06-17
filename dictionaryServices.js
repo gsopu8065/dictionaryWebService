25c9e112129230ebad7aa973d58f060ff235f6d6
srujangopu <srujanjack@gmail.com>
2016-06-16 21:41:50 -0500

    added two services

--- dictionaryServices.js
+++ dictionaryServices.js
@@ -4,6 +4,20 @@
 var app = require('./main.js')
 var urban = require('urban');
 
+app.get('/randomWords', function (req, res) {
+    var list =[]
+    urban.random().first(function(json){
+        list.push(json)
+        urban.random().first(function(json){
+            list.push(json)
+            urban.random().first(function(json){
+                list.push(json)
+                res.send(list)
+            })
+        })
+    })
+});
+
 app.get('/random', function (req, res) {
     urban.random().first(function(json) {
         res.send(json)
