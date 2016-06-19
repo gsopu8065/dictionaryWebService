c101b6bf04fed9750b763fbabf717e6264422740
srujangopu <srujanjack@gmail.com>
2016-06-19 17:04:37 -0500

    mongodb work done

--- dictionaryServices.js
+++ dictionaryServices.js
@@ -1,29 +1,30 @@
 /**
  * Created by srujangopu on 6/14/16.
  */
-var app = require('./main.js')
+var app = require('./main.js');
 var urban = require('urban');
-
-app.get('/randomWords', function (req, res) {
-    var list =[]
-    urban.random().first(function(json){
-        json.source = "Urban Dictionary"
-        list.push(json)
-        urban.random().first(function(json){
-            json.source = "Urban Dictionary"
-            list.push(json)
-            urban.random().first(function(json){
-                json.source = "Urban Dictionary"
-                list.push(json)
-                res.send(list)
-            })
-        })
-    })
-});
+var dbServices = require('./mongodb/dbServices.js')
 
 app.get('/random', function (req, res) {
-    urban.random().first(function(json) {
+    urban.random().first(function (json) {
         json.source = "Urban Dictionary"
         res.send(json)
     });
+});
+
+app.post('/signup', function (req, res) {
+    dbServices.signup(req.body, res)
+});
+
+app.get('/getEmail', function (req, res) {
+    dbServices.getEmail(req.query.deviceId, res)
+    //res.send("hello world")
+});
+
+app.post('/saveWord', function (req, res) {
+    dbServices.saveWord(req.body, res)
+});
+
+app.get('/getSavedWords', function (req, res) {
+    dbServices.getSavedWords(req.query.email, res)
 });
---
+++ mongodb/dbServices.js
@@ -0,0 +1,53 @@
+/**
+ * Created by srujangopu on 6/19/16.
+ */
+var mongodb = require('mongodb');
+var mongoDbConnection = require('./connection.js');
+
+var dbservice = {
+
+    signup: function(body, res){
+
+        mongoDbConnection(function (databaseConnection) {
+            databaseConnection.collection('dictionary', function (error, collection) {
+                collection.insert(body, function (err, records) {
+                    res.send(records)
+                })
+            });
+        });
+    },
+
+    getEmail: function(deviceId, res){
+
+        mongoDbConnection(function (databaseConnection) {
+            databaseConnection.collection('dictionary', function (error, collection) {
+                collection.find({_id: deviceId}).toArray(function(err, dbres) {
+                    res.jsonp(dbres);
+                });
+            });
+        });
+
+    },
+
+    saveWord: function(body, res){
+        mongoDbConnection(function (databaseConnection) {
+            databaseConnection.collection('dictionary', function (error, collection) {
+                collection.update({email: body.email}, {$push: {words: {$each: body.word}}}, function (err, records) {
+                    res.send(records)
+                })
+            });
+        });
+    },
+
+    getSavedWords : function(email, res){
+        mongoDbConnection(function (databaseConnection) {
+            databaseConnection.collection('dictionary', function (error, collection) {
+                collection.find({email: email}).next(function (err, doc) {
+                    res.send(doc)
+                })
+            });
+        });
+    }
+}
+
+module.exports = dbservice;
